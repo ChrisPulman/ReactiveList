0648b0540329648d4f5f8035f565153c06f22996
Chris Pulman <chris.pulman@yahoo.com>
2023-10-07 06:19:44 +0100

    Add Tests

---
+++ src/ReactiveList.Test/TestData.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Chris Pulman. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace ReactiveList.Test
+{
+    internal class TestData(string name, int age)
+    {
+        public int Age { get; } = age;
+
+        public string Name { get; } = name;
+    }
+}
